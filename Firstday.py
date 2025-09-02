49cfa82e33e809f13fa27e60c201061a58c0f631
pranithkumar11 <praneeth.dupukuntla.com>
2025-09-02 23:37:04 +0530

    first step

---
+++ Firstday.py
@@ -0,0 +1,46 @@
+print("Hello world")
+print(3+5)
+x = 10
+print(type(x))
+print(type(3.2))
+print(type(({"apple", "banana", "cherry"})))
+print(type(True))
+name = "pranith"# string
+age = -22 # int
+height = 5.9 # float
+print(age)
+print(height)
+print(name)
+print("My name is " + name + ", my age is " + str(age) + ", and my height is " + str(height) )
+print("my age is " + str(age))
+age2=age
+print(age2) 
+print((age))
+print (type(True))
+print(type(False))
+a = 10
+b = 20
+c  = a + b
+print(c)
+print(a+b)
+print(a-b)
+print(type(b))
+print("first + a * b = " + str(a*b+c))
+A,B = 2,3
+print(A*B)
+c = 10
+print(A+b)
+print(A+b+c)
+a,b=1,2
+c= a/b
+print(c)
+c1 = a//b
+print(c1)
+a,b = 12,5
+c= a/b
+print(c)
+c1 = a//b
+print(c1)  
+a,b=-10,5
+print(a/b)
+print(a//b)
